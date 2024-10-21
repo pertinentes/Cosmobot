c16325fa95c4019852208c04479c260459e457d6
æ <143338094+pertinentes@users.noreply.github.com>
2024-10-21 13:13:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    token: "",
-    prefix: "&",
-    owner: "",
+    token: "", //ton token
+    prefix: "&", // ton prefix
+    owner: "", // ton id
     support: "https://discord.gg/",
     images: {
-        banner: "https://i.imgur.com/RIXRLvL.png",
-        icon: "https://i.imgur.com/Gbw1upZ.gif"
-    }
+        banner: "https://i.imgur.com/RIXRLvL.png", // bannière de l'ancien help 
+        icon: "https://i.imgur.com/Gbw1upZ.gif" // icon des footer et thumbnail
+    }, 
+    imgur: "", // client id imgur pour les logs messages (facultatif) https://api.imgur.com/oauth2/addclient
 }
